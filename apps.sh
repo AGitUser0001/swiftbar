28732bbc76ede69eca9014b785aa5c66a987a8b8
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-13 20:37:34 -0400

    add initial update

--- apps.sh
+++ apps.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh -f
 # <swiftbar.type>streamable</swiftbar.type>
 # <swiftbar.runInBash>false</swiftbar.runInBash>
+echo ':chevron.down:'
 while (( 1 )) {
   text="$( osascript -e 'set text item delimiters to "\n"
   tell application "System Events" to get the title of every window of every application process as text' | sed -e '/^$/d' -e 's/$/ | symbolize=false emojize=false/' )"
