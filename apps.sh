e0158c4bb3c13337cadecfc30adaa8add6e54d68
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2024-03-13 20:36:50 -0400

    add apps plugin

---
+++ apps.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh -f
+# <swiftbar.type>streamable</swiftbar.type>
+# <swiftbar.runInBash>false</swiftbar.runInBash>
+while (( 1 )) {
+  text="$( osascript -e 'set text item delimiters to "\n"
+  tell application "System Events" to get the title of every window of every application process as text' | sed -e '/^$/d' -e 's/$/ | symbolize=false emojize=false/' )"
+  echo '~~~'
+  echo ':chevron.down:'
+  echo "$text"
+  sleep 1
+}
